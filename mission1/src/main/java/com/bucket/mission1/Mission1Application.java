bb8bb177b759b8dcdcebe5c1dfb45579c12801c7
nattaya-ch <nattaya-ch@users.noreply.github.com>
2020-04-24 15:31:19 +0700

    Second commit

--- mission1/src/main/java/com/bucket/mission1/Mission1Application.java
+++ mission1/src/main/java/com/bucket/mission1/Mission1Application.java
@@ -1,10 +1,13 @@
 package com.bucket.mission1;
 
-
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Mission1Application {
+
 	public static void main(String[] args) {
-		SpringApplication.run(HelloWorldController.class, args);
+		SpringApplication.run(Mission1Application.class, args);
 	}
+
 }
